38096e4a2671f8a84d0b38e7b5b28d99805d5244
Jeremiah Stacey <jeremiah.j.stacey@gmail.com>
2020-06-07 18:57:09 -0500

    Test Path Resolution Updates
    Blank file requests now result in exceptions.  Updating this test to
    adjust the request parameter to allow the ResourceLoader to do it's job.

--- src/test/java/org/owasp/esapi/waf/MockWafServletContext.java
+++ src/test/java/org/owasp/esapi/waf/MockWafServletContext.java
@@ -1,14 +1,16 @@
 package org.owasp.esapi.waf;
 
+import java.io.File;
+
 import org.owasp.esapi.ESAPI;
 import org.owasp.esapi.http.MockServletContext;
 
 public class MockWafServletContext extends MockServletContext {
 
 	public String getRealPath(String s) {
-		
-		return ESAPI.securityConfiguration().getResourceFile( "" ).getAbsolutePath() + "/" + s;
-		
+	    String resourceLookup = s.endsWith("log4j.xml") ? "log4j.xml" : s;
+        File resource = ESAPI.securityConfiguration().getResourceFile(resourceLookup);
+        return resource.exists() && resource.canRead() ? resource.getAbsolutePath() : null;
 	}
 	
 }
